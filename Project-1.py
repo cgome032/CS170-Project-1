3a73a0014e8a9a1bcf17809d4fcc3f305a3ba9a4
Carlos Gomez <cgome032@ucr.edu>
2018-10-29 23:43:53 -0700

    Finished most of the problem class and node class, need to make sure the initial state is being passed into the set for uniqueness

--- Project-1.py
+++ Project-1.py
@@ -1,4 +1,5 @@
 import copy as cp
+import queue
 
 description  = "Welcome to Carlos Gomez's 8-puzzle solver.\n"
 description += 'Type "1" to use a default puzzle, or "2" to enter your own puzzle.\n'
@@ -20,100 +21,125 @@ class Node:
     def __init__(self, state):
         self.state = state
 
+    def __repr__(self):
+        return self.state
+
     def get_state(self):
         return self.state
+    
 
 class Problem:
 
-    def __init__(self, input_array):
-        self.input_array = input_array
+    def __init__(self, initial_state):
+        self.initial_state = initial_state
         self.map_attempts = set()
         self.goal_state = [1,2,3,4,5,6,7,8,0]
 
-    def goal_test(self):
-        return self.goal_state == self.input_array
+    def goal_test(self, node_state):
+        return self.goal_state == node_state
 
-    def diff_test(self):
+    def diff_test(self, curr_state):
         total_diff = 0
-        for goal_val, input_val in zip(self.goal_state, self.input_array):
+        for goal_val, input_val in zip(self.goal_state, curr_state):
             if goal_val != input_val:
                 total_diff += 1
         return total_diff
 
     def test_attempt(self, node_state):
-        if node_state in self.map_attempts:
+        if tuple(node_state) in self.map_attempts:
             return False
         else:
-            self.map_attempts.add(node_state)
+            self.map_attempts.add(tuple(node_state))
             return True
-    def find_blank(self):
-        for index, value in enumerate(self.input_array):
+
+    def find_blank(self, curr_state):
+        for index, value in enumerate(curr_state):
             if value == 0:
                 return index
 
-    def operators(self):
+    def operators(self, curr_state):
         operator_list = list()
-        zero_index = self.find_blank()
+        zero_index = self.find_blank(curr_state)
         
         # Move up allowed
         if zero_index > 2:
-            operator_list.append(self.move_up(zero_index))
+            up_node = Node(self.move_up(zero_index, curr_state))
+            if self.test_attempt(up_node.get_state()):
+                operator_list.append(up_node)
 
         # Move down allowed
         if zero_index < 6:
-            operator_list.append(self.move_down(zero_index))
+            down_node = Node(self.move_down(zero_index, curr_state))
+            if self.test_attempt(down_node.get_state()):
+                operator_list.append(down_node)
 
         # Move left allowed
         if zero_index % 3 > 0:
-            operator_list.append(self.move_left(zero_index))
+            left_node = Node(self.move_left(zero_index, curr_state))
+            if self.test_attempt(left_node.get_state()):
+                operator_list.append(left_node)
 
         # Move right allowed
         if zero_index % 3 < 2:
-            operator_list.append(self.move_right(zero_index))
+            right_node = Node(self.move_right(zero_index, curr_state))
+            if self.test_attempt(right_node.get_state()):
+                operator_list.append(right_node)
         
         return operator_list
 
-    def move_up(self, index):
-        up_list = cp.deepcopy(self.input_array)
+    def move_up(self, index, curr_state):
+        up_list = cp.deepcopy(self.initial_state)
         up_list[index], up_list[index-3] = up_list[index-3], up_list[index]
         return up_list
 
-    def move_down(self, index):
-        down_list = cp.deepcopy(self.input_array)
+    def move_down(self, index, curr_state):
+        down_list = cp.deepcopy(self.initial_state)
         down_list[index], down_list[index+3] = down_list[index+3], down_list[index]
         return down_list
 
-    def move_left(self, index):
-        left_list = cp.deepcopy(self.input_array)
+    def move_left(self, index, curr_state):
+        left_list = cp.deepcopy(self.initial_state)
         left_list[index], left_list[index-1] = left_list[index-1], left_list[index]
         return left_list
 
-    def move_right(self, index):
-        right_list = cp.deepcopy(self.input_array)
+    def move_right(self, index, curr_state):
+        right_list = cp.deepcopy(self.initial_state)
         right_list[index], right_list[index+1] = right_list[index+1], right_list[index]
         return right_list
 
     def state(self):
-        return self.input_array
+        return self.initial_state
 
 def uniform(nodes, new_nodes):
-    pass
-
-def expand(node, problem_operators):
-    new_nodes = list()
+    newQueue = nodes
+    for node in new_nodes:
+        newQueue.put(node)
+    return newQueue
 
 
 def general_search(problem, q_function):
-    print('1')
+    nodes = queue.Queue()
+    first_node = Node(problem.state())
+    nodes.put(first_node)
+
+    while(1):
+        if nodes.empty():
+            return "failure"
+        node = nodes.get()
+        tile_print(node.get_state())
+        if problem.goal_test(node.get_state()):
+            return node
+        nodes = q_function(nodes, problem.operators(node.get_state()))
+
+def tile_print(input_list):
+    for index, tile in enumerate(input_list):
+        print(tile, end=' ')
+        if index % 3 == 2:
+            print()
+    print()
 
 if __name__ == '__main__':
-    test_val = [1,2,3,4,5,6,7,8,0]
+    test_val = [1, 2, 3, 4, 0, 6, 7, 5, 8]
     test = Problem(test_val)
-    test.find_blank()
-    test_list = test.operators()
-    for single_list in test_list:
-        for index, value in enumerate(single_list):
-            print(value, end=' ')
-            if index % 3 == 2:
-                print()
-        print()
+    node = general_search(test, uniform)
+    print(node)
